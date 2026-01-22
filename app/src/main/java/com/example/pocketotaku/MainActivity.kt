fd6a2e0ed6073dc0933245978ada237f52f74d6e
rohit.vishwas <Rohit.Vishwas@ril.com>
2026-01-22 13:55:34 +0530

    Add gradle file

---
+++ app/src/main/java/com/example/pocketotaku/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.pocketotaku
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.example.pocketotaku.ui.navigation.NavGraph
+import com.example.pocketotaku.ui.theme.PocketOtakuTheme
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            PocketOtakuTheme {
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
